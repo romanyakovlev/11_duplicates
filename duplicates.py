66dfa541f6c8ecd927a100cd5411b9f1298cc3b7
romanyakovlev <yakovlevroman8@gmail.com>
2016-10-14 22:27:38 +0700

    added file list for printing at screen

--- duplicates.py
+++ duplicates.py
@@ -4,31 +4,50 @@ from collections import Counter
 import os
 import argparse
 
-def are_files_duplicates(dir_name):
+def list_of_repeated_files_fun(dir_name):
 
     list_with_all_files = []
+    list_with_all_files_with_path = []
+    path = []
 
-    for current_folder, dirs, files in os.walk(dir_name):
-        for current_file in os.scandir(current_folder):
-                if current_file.is_file():
-                    list_with_all_files.append((current_file.name, current_file.stat().st_size))
+    for root_dir, folder_names, file_names in os.walk(dir_name):
+        for file_name in file_names:
+            file_path = '/'.join([root_dir,file_name])
+            file_size = os.path.getsize(file_path)
+            list_with_all_files.append((file_name, file_size))
+            path.append(file_path)
+
+    list_with_all_files_with_path = list(zip(list_with_all_files,path))
 
     list_with_file_counts = []
 
-    for file_name, repeat_count in Counter(tuple(list_with_all_files)).items():
-        if repeat_count>1:
-            list_with_file_counts.append(file_name[0])
+    repeat_count_list = Counter(tuple(list_with_all_files)).items()
 
-    if list_with_file_counts == []:
-        list_with_file_counts.append(u'Ни один файл не повторяется')
+    dictir = {}
 
-    return list_with_file_counts
+    for x in repeat_count_list:
+        dictir[x[0]]=[y[-1] for y in list_with_all_files_with_path if x[0]==y[0]]
+
+    for file_name in repeat_count_list:
+        if file_name[1] > 1:
+            list_with_file_counts.append(file_name)
+
+    return list_with_file_counts, dictir
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('dir_name')
     dir_name = parser.parse_args().dir_name
 
     print('\nФайлы, повторяющиеся больше одного раза:\n')
-    for repeated_file in are_files_duplicates(dir_name):
-        print(repeated_file)
+
+    list_of_repeated_files,dictir = list_of_repeated_files_fun(dir_name)
+
+    if not list_of_repeated_files:
+        print('Ни один файл не повторяется')
+    else:
+        for repeated_file in list_of_repeated_files:
+            print('Файл "{}" повторяется {} раз:'.format(repeated_file[0][0],repeated_file[1]))
+            for z in dictir[repeated_file[0]]:
+                print(z)
